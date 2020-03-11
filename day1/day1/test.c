a6250c57375f0692a8a215863521dc0ebe8af6b2
wagnji <1074466427@qq.com>
2020-03-11 13:57:28 +0800

    第一天代码

---
+++ day1/day1/test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	/*printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(double));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n",sizeof(long double));*/
+//
+//	/*printf("%c\n", '\'');
+//	printf("%s\n", "\"");*/
+//
+//	//printf("%d\n", strlen("abcdef"));
+//	//printf("%d\n", strlen("c:\test\328\test.c"));  //14
+//
+//
+//
+//	return 0;
+//}
+//void fun()
+//{
+//		int i = 0;
+//		i++;
+//		printf("%d\n", i);
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		fun();
+//	}
+//}
+
+//void fun()
+//{
+//	static int i = 0;
+//	i++;
+//	printf("%d\n", i);
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		fun();
+//	}
+//}
+
+//#define MAX 1000;
+//#define ADD(x, y) ((x)+(y));  //宏定义 
+//
+//int main()
+//{
+//	int sum = ADD(2,3);
+//	printf("%d\n", sum);
+//	sum = 10 * ADD(2, 3);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	&num;
+//	printf("%p\n", &num);  //%p 以地址的形式打印
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(int*));
+//	return 0;
+//}
+

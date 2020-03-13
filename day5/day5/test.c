58c7151d3ba3c5c6a50899b63548b323e3028da9
wagnji <1074466427@qq.com>
2020-03-13 19:00:19 +0800

    第五天

---
+++ day5/day5/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 2));
+//	return 0;
+//}
+
+//int my_strlen(const char* str)   //strlen 函数的实现
+//{
+//	if (*str == '\0')
+//		return 0;
+//	else
+//		return 1 + my_strlen(str + 1);
+//}
+//
+//int main()
+//{
+//	char* str = "abcdef";
+//	int ret = my_strlen(str);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+int main()
+{
+	char* str = "abcdef";
+}

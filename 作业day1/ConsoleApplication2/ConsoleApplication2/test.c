fdbc807830ef65ae84ca30ddc368313fbeb87b3e
wagnji <1074466427@qq.com>
2020-03-18 19:00:25 +0800

    练习

---
+++ 作业day1/ConsoleApplication2/ConsoleApplication2/test.c
@@ -0,0 +1,32 @@
+
+#define P 7
+#define NULL_DATA -1
+struct bucket_node
+{
+	int data[3];
+	struct bucket_node *next;
+};
+struct bucket_node hash_table[P];
+int hash
+
+int insert_new_element(int new_element)
+{
+	for(int i=0; i<3; ++i)
+	return 0;
+}
+
+void hash_init()
+{
+	for(int i=0; i<P; ++i)
+	{
+		for(int j=0; j<3; ++j)
+		{
+
+		}
+	}
+}
+
+int main()
+{
+
+}

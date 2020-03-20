b125c60a64d3ed67febc7d32436150d161b3e804
wagnji <1074466427@qq.com>
2020-03-20 12:07:33 +0800

    练习

---
+++ 作业day1/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+int main() 
+{
+	printf("Received message:Hello,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("Received message:shhjfegguf,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("Received message:fhjehgsfhjedg,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("Received message:sfghehfsusfwasde,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+} 
+
+

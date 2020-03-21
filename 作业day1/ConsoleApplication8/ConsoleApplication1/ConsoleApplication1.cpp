31ae86e3ec9ed0b7cc36833fd058209d256b1634
wagnji <1074466427@qq.com>
2020-03-21 23:26:46 +0800

    练习

---
+++ 作业day1/ConsoleApplication8/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <stdio.h>
+int main() 
+{
+	printf("Received message:Hello,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("Received message:shhjfegguf,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("Received message:fhjehgsfhjedg,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("Received message:sfghehfsusfwasde,from client:192.168.0.100\n");
+	printf("Bytes sent:31\n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+} 
+
+
---
+++ 作业day1/ConsoleApplication8/ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,50 @@
+// ConsoleApplication8.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+#include <stdio.h>
+int main() 
+{
+	printf("Hello\n");
+	printf("Sent bytes:5\n");
+	printf("Connection closed\n");
+	printf("shhjfegguf                 \n");
+	printf("Sent bytes:10                 \n");
+	printf("Connection closed\n");
+	printf("fhjehgsfhjedg                 \n");
+		printf("Sent bytes:13                 \n");
+	printf(" Connection closed                \n");
+	printf("sfghehfsusfwasde                 \n");
+	printf(" Sent bytes:16                \n");
+		printf("Connection closed                  \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+		printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+	printf("                 \n");
+} 
+

cb21d5d4a85978b8615f32e33647a658c2ea142e
wagnji <1074466427@qq.com>
2020-04-04 20:59:22 +0800

    练习

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,7 @@
+// ConsoleApplication3.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+
---
+++ ConsoleApplication3/ConsoleApplication3/源.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdafx.h>
+
+
+int main()
+{
+	printf("hello world\n");
+	
+}

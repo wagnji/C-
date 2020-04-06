665660e50cdad988a98b2b730798820e9542f8a5
wagnji <1074466427@qq.com>
2020-04-06 20:35:59 +0800

    递归练习

---
+++ 递归1/递归1/递归1.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//int fun(int n)
+//{
+//	if (n == 0 || n == 1)
+//		return 1;
+//	return fun(n - 1) + fun(n - 2);
+//}
+//
+//int main()
+//{
+//	int n;
+//	scanf_s("%d", &n);
+//	printf("n的斐波拉契数为%d", fun(n));
+//	system("pause");
+//}
+
+//int fun(int m, int n)
+//{
+//	if (n == 0)
+//		return m;
+//	fun(n, m % n);
+//}
+//
+//int main()
+//{
+//	int m;
+//	int n;
+//	scanf_s("%d%d", &m, &n);
+//	printf("m和n的最大公约数是%d", fun(m, n));
+//	system("pause");
+//}
+int fun(int n);
+{
+   
+}
+
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	printf("组成数字之和为%d", &fun(n));
+	system("pause");
+}
